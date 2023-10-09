598586b84419b625f0e097da3ab0af5d182024bd
phurin prasit <phus.2644@gmail.com>
2023-10-09 18:01:45 +0700

    .

--- src/app/api/student/route.ts
+++ src/app/api/student/route.ts
@@ -1,6 +1,7 @@
 import { getPrisma } from "@/libs/getPrisma";
 import { Student } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+
 export const dynamic = "force-dynamic";
 
 export type StudentGetResponse = {
@@ -9,12 +10,12 @@ export type StudentGetResponse = {
 
 export const GET = async () => {
   const prisma = getPrisma();
-  const task = await prisma.student.findMany()
-  //2. Display list of student
-  // const students = await prisma...
+  
+  // Fetch a list of students from the database
+  const students = await prisma.student.findMany();
 
   return NextResponse.json<StudentGetResponse>({
-    students: [], //replace empty array with result from DB
+    students,
   });
 };
 
@@ -33,13 +34,30 @@ export const POST = async (request: NextRequest) => {
   const body = (await request.json()) as StudentPostBody;
   const prisma = getPrisma();
 
-  //4. Add new Student data
-  // await prisma...
+  // Check if a student with the same studentId already exists
+  const existingStudent = await prisma.student.findUnique({
+    where: {
+      studentId: body.studentId,
+    },
+  });
 
-  // return NextResponse.json<StudentPostErrorResponse>(
-  //   { ok: false, message: "Student Id already exists" },
-  //   { status: 400 }
-  // );
+  if (existingStudent) {
+    // Return an error response if the studentId already exists
+    return NextResponse.json<StudentPostErrorResponse>(
+      { ok: false, message: "Student Id already exists" },
+      { status: 400 }
+    );
+  }
 
-  // return NextResponse.json<StudentPostOKResponse>({ ok: true });
+  // If the studentId doesn't exist, create a new student
+  const newStudent = await prisma.student.create({
+    data: {
+      studentId: body.studentId,
+      firstName: body.firstName,
+      lastName: body.lastName,
+    },
+  });
+
+  // Return a success response if the student is created successfully
+  return NextResponse.json<StudentPostOKResponse>({ ok: true });
 };
